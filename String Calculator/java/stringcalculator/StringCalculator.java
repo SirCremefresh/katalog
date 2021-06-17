8844ff62f54943c9fe07e3cfa1246ab05d3da292
donato <donato@wolfisberg.dev>
2021-06-17 14:34:17 +0200

    returnOneOnOneString

--- String Calculator/java/stringcalculator/StringCalculator.java
+++ String Calculator/java/stringcalculator/StringCalculator.java
@@ -2,6 +2,9 @@ package stringcalculator;
 
 public class StringCalculator {
 	public int calculate(String input) {
+		if(input.equals("1")) {
+			return 1;
+		}
 		return 0;
 	}
 }
--- String Calculator/test/stringcalculator/StringCalculatorShould.java
+++ String Calculator/test/stringcalculator/StringCalculatorShould.java
@@ -15,4 +15,14 @@ public class StringCalculatorShould {
 		Assertions.assertEquals(0, output);
 	}
 
+	@Test
+	void returnOneOnOneString() {
+		String input = "1";
+		StringCalculator calculator = new StringCalculator();
+
+		int output = calculator.calculate(input);
+
+		Assertions.assertEquals(1, output);
+	}
+
 }
